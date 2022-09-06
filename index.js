12f16a90248ca5417f7d0ef7a0024521d737e10d
STUDENT\2055171 <NCS.OWGJ@gmail.com>
2022-09-06 16:00:16 +0900

    index.jsのloginを書き換えました。

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const Discord = require("discord.js")
+const client = new Discord.Client()
 
-// メッセージ取得
-client.on('message', msg => {
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on("message", msg => {
   if (msg.content === 'test') {
     msg.reply('Bot呼び出しテストです。こんにちは、'+msg.author.username+'さん。');
   }
-});
+})
 
-client.login('トークン');
+client.login(process.env.TOKEN)
