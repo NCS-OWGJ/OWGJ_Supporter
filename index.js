7ab99a87f0f8bab0dcd6fc8c34312556fe9a9574
itota37 <40158522+itota37@users.noreply.github.com>
2022-09-08 13:25:11 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// OWGJ_Supporter
+// OWGJをサポートします。
 
 const Discord = require("discord.js")
 const client = new Discord.Client()
@@ -7,9 +9,10 @@ client.on("ready", () => {
 })
 
 client.on("message", msg => {
-  if (msg.content === 'test') {
-    msg.reply('Bot呼び出しテストです。こんにちは、'+msg.author.username+'さん。');
+  if (msg.content === 'help') {
+    msg.reply('現在有用なコマンドはありません。');
   }
 })
 
-client.login(process.env.TOKEN)
+client.login("MTAxNjkxMzQzODM4MDkyMDg4Mg.GmZXVu.elgIlvt7fF39Pz0AH-Nu8pIsT3znVoqEYR_2lg")
+//client.login(process.env.TOKEN)
